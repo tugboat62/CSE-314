f9dc9cee72aa234c7322dd90396d87c2781d58ed
tugboat62 <mujadded1998@gmail.com>
2023-02-02 19:19:47 +0600

    updated

---
+++ zemaphore/test-toggle.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <wait.h>
+#include <pthread.h>
+#include "zemaphore.h"
+
+#define NUM_THREADS 3
+#define NUM_ITER 10
+
+zem_t child;
+zem_t parent;
+
+void *justprint(void *data)
+{
+  int thread_id = *((int *)data);
+
+  for (int i = 0; i < NUM_ITER; i++)
+  {
+    zem_down(&child);
+    printf("This is thread %d\n", thread_id);
+    zem_up(&parent);
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+  pthread_t mythreads[NUM_THREADS];
+  int mythread_id[NUM_THREADS];
+
+  zem_init(&child, 0);
+  zem_init(&parent, 0);
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    mythread_id[i] = i;
+    pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
+  }
+
+  for (int i = 0; i < NUM_ITER * NUM_THREADS; i++)
+  {
+    zem_up(&child);
+    zem_down(&parent);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    pthread_join(mythreads[i], NULL);
+  }
+
+  return 0;
+}
